6058f3abb750f79c795131a2bc03b12f07ae955a
Iranaphor <Primordia@live.com>
2019-11-18 13:51:48 +0000

    Automatic Commit

--- DOWNLOADER.sh
+++ DOWNLOADER.sh
@@ -2,11 +2,14 @@
 
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get purge "*gazebo*"
-sudo apt-get install ros-kinetic-uol-cmp9767m-base
-sudo apt-get install ros-kinetic-uol-cmp9767m-tutorial
-sudo apt-get install ros-kinetic-image-view
-sudo apt-get install ros-kinetic-rqt-graph
-sudo apt-get install ros-kinetic-rviz
+#sudo apt-get install \
+#	ros-kinetic-uol-cmp9767m-base \
+#	ros-kinetic-uol-cmp9767m-tutorial
+
+sudo apt-get install \
+	ros-kinetic-image-view \
+	ros-kinetic-rqt-graph \
+	ros-kinetic-rviz
 
 sudo apt-get install \
     ros-kinetic-robot-localization \
